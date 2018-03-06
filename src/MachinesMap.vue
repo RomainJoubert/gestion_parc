f0cf714d804e6c590260763675839b3f14c37edc
RomainJoubert <romain.joubert@Digital-Grenoble.local>
2018-03-06 17:14:11 +0100

    ajout carte

--- src/MachinesMap.vue
+++ src/MachinesMap.vue
@@ -1,7 +1,11 @@
 <template>
-	
+	<div>
 		<h1>Carte des machines</h1>
-	
+		<gmap-map class="maps"
+  :center="{lat:45.1885, lng:5.7245}"
+  :zoom="12">
+</gmap-map>
+	</div>
 </template>
 
 <script>
@@ -11,3 +15,13 @@
 	}
 
 </script>
+
+<style>
+.maps{
+	width: 80%; 
+	height: 800px; 
+	margin: auto;
+	border: solid 5px;
+	border-radius: 10px 10px;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import Liste from'./MachineList.vue'
 import Carte from'./MachinesMap.vue'
 import VueRouter from 'vue-router'
 import Machine from './Machine.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: ' AIzaSyC4_FpRR1CR-F1LpZeuIITANV77JO0LywI',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+  }
+})
 
 Vue.component('liste', Liste) //en premier le nom que je définis et que j'appellerai dans ma balise, en 2ème le nom de la props
 Vue.component('carte', Carte)
